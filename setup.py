23fd56d0839a4e63cb2094d7ef466b7445ab55b3
Abdur Rahman <airdipu@gmail.com>
2025-04-22 15:06:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="smear_data_api",
-    version="1.0.1",
+    version="1.0.2",
     description="A Python package for interacting with the SMEAR API and processing data for continuous period.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Abdur Rahman",
     author_email="abdur.rahman@helsinki.fi",
     url="https://github.com/airdipu/SMEAR-data-api",
