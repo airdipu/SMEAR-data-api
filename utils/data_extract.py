dcfe037a458a81344ab66e6a410bfa0a87445bad
Abdur Rahman <airdipu@gmail.com>
2025-04-22 13:03:32 +0300

    update
    updated smear

--- utils/data_extract.py
+++ utils/data_extract.py
@@ -1,30 +1,30 @@
 import pandas as pd
-import smear.smear
+import smear.smear_fetch
 
 def fetch_combined_data(variables, start_year, end_year, quality='ANY', averaging='1', avg_type='NONE'):
     """
     Fetch data for the specified variables from the SMEAR API over a range of years and combine into a single DataFrame.
 
     Args:
-    variables (list): List of variables to fetch.
-    start_year (int): The starting year for the data fetch.
-    end_year (int): The ending year for the data fetch.
-    quality (str): The quality of data to fetch. Default is 'ANY'.
-    averaging (str): The averaging interval in minutes. Default is '1'.
-    avg_type (str): The type of averaging. Default is 'NONE'.
+    - variables (list): List of variables to fetch.
+    - start_year (int): The starting year for the data fetch.
+    - end_year (int): The ending year for the data fetch.
+    - quality (str): The quality of data to fetch. Default is 'ANY'.
+    - averaging (str): The averaging interval in minutes. Default is '1'.
+    - avg_type (str): The type of averaging. Default is 'NONE'.
 
     Returns:
-    pd.DataFrame: Combined data for the specified range.
+    - pd.DataFrame: Combined data for the specified range.
     """
 
     combined_data = pd.DataFrame()
 
     for year in range(start_year, end_year + 1, 2):
 
-        start_date = f"{year}-01-01"     # Need to modify for the date
+        start_date = f"{year}-01-01"
         end_date = f"{min(year + 1, end_year)}-12-31"
 
-        data_segment = smear.smear.getData(variables, start=start_date, end=end_date, 
+        data_segment = smear.smear_fetch.getData(variables, start=start_date, end=end_date, 
                                                quality=quality, averaging=averaging, avg_type=avg_type)
         
         if not isinstance(data_segment, pd.DataFrame):
@@ -36,7 +36,6 @@ def fetch_combined_data(variables, start_year, end_year, quality='ANY', averagin
 
         combined_data = pd.concat([combined_data, data_segment])
 
-    # Sort the combined data by the index (time) to maintain chronological order
     combined_data.sort_index(inplace=True)
 
     return combined_data
